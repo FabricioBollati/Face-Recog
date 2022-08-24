d4c757fcad4425a13c8e92776e05951c6c3f821c
Yosi <neugrashxsteam@hotmail.com>
2022-08-24 17:58:07 -0300

    PC

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Clarifai from 'clarifai';
 import Signin from './Components/Signin/Signin.js';
 import Register from './Components/Register/Register.js';
 
+
+
 const app = new Clarifai.App({
   apiKey: 'd97de0922e1f4154bd59612dd5cd1f3c'
 });
@@ -22,9 +24,32 @@ class App extends Component {
       imgUrl: '',
       box: {},
       route: 'Signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
+
+  loadUser = (data) => {
+    this.setState({ user:{
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined}
+    })
+  }
+
+  // componentDidMount() {
+  //   fetch('http:/localhost:3000')
+  //     .then(response => response.json())
+  //     .then(console.log)
+  // }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
@@ -47,15 +72,30 @@ class App extends Component {
     this.setState({input: event.target.value})
   }
 
-  onButtonSubmit = (event) => {
+  onButtonSubmit = () => {
     this.setState({imgUrl: this.state.input});
-
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
-      .then(response =>  this.displayFaceBox(this.calculateFaceLocation(response)))
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                id: this.state.user.id
+              })
+            })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, {entries: count}))
+            })
+          }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+        })
       .catch(err => console.log(err));
-  }
+    }
 
   onRouteChange = (route) => {
     if (route == 'SignOut') {
@@ -75,7 +115,7 @@ class App extends Component {
         { this.state.route == 'home' 
           ? <div> 
               <Logo  />
-              <Rank/>
+              <Rank name={this.state.user.name} entries={this.state.user.entries}/>
               <ImageLinkForm 
                 onInputChange={this.onInputChange} 
                 onButtonSubmit={this.onButtonSubmit}/>
@@ -83,8 +123,8 @@ class App extends Component {
             </div> 
           : ( 
             route == 'Signin' 
-              ? <Signin onRouteChange={this.onRouteChange} />
-              : <Register onRouteChange={this.onRouteChange} /> 
+              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} /> 
             )
         }
       </div>
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,36 +1,85 @@
 import react from "react";
 
-const Register = ({onRouteChange}) => {
-    return (
-        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-            <main className="pa4 black-80">
-                <div className="measure">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                    <legend className="f1 fw6 ph0 mh0">Register</legend>
-                    <div className="mt3">
-                        <label className="db fw6 lh-copy f6" htmlFor="email-address">Name</label>
-                        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name"/>
+class Register extends react.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            registerEmail:"",
+            registerPassword:"",
+            registerName:""
+        }
+    }
+
+    onNameChange = (event) => {
+        this.setState({registerName: event.target.value})
+    }
+
+    onEmailChange = (event) => {
+        this.setState({registerEmail: event.target.value})
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({registerPassword: event.target.value})
+    }
+
+    onSubmitRegister = () => {
+        fetch('http://localhost:3000/register', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: this.state.registerEmail,
+                password: this.state.registerPassword,
+                name: this.state.registerName
+            })
+        })
+        .then(response => response.json())
+        .then(console.log)
+        .then(user => {
+            if(user) {
+                this.props.loadUser(user);
+                this.props.onRouteChange('Signin');
+            }
+        })
+    }
+    // data => {
+    //     if(data === 'success') {
+    //         this.props.onRouteChange('signin')
+    //     }
+    // }
+
+    render(){
+        const {onRouteChange} = this.props;
+        return (
+            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+                <main className="pa4 black-80">
+                    <div className="measure">
+                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                        <legend className="f1 fw6 ph0 mh0">Register</legend>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Name</label>
+                            <input onChange={this.onNameChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name"/>
+                        </div>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                            <input onChange={this.onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+                        </div>
+                        <div className="mv3">
+                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                            <input onChange={this.onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
+                        </div>
+                        </fieldset>
+                        <div onClick={() => onRouteChange('Signin')} className="">
+                        <input  
+                            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                            type="submit" 
+                            value="Register"
+                            onClick={this.onSubmitRegister}/>
+                        </div>
                     </div>
-                    <div className="mt3">
-                        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
-                    </div>
-                    <div className="mv3">
-                        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
-                    </div>
-                    </fieldset>
-                    <div className="">
-                    <input  
-                        className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                        type="submit" 
-                        value="Register"
-                        onClick={() => onRouteChange('home')}/>
-                    </div>
-                </div>
-            </main>
-        </article>
-    )
+                </main>
+            </article>
+        )
+    }
 }
 
 export default Register
